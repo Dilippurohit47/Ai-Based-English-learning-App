35b6de49d11c3eace698393821608717adea63f6
Dilippurohit47 <dilippurohit204@gmail.com>
2024-09-30 15:49:05 +0530

    docs page on landing page

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,16 +1,14 @@
 "use client";
-import React, { useState } from "react";
-import { SlideDown } from "react-slidedown";
 import "react-slidedown/lib/slidedown.css";
 
-import DocsSingleBox from "../../components/my-components/DocsSingleBox"
 import { Documents } from "@/lib/constant";
+import DocsSingleBox from "../../components/my-components/DocsSingleBox";
 
 
 const page = () => {
 
   return (
-    <div className=" items-center  px-11 py-14 text-2xl same-bg mt-16 justify-center">
+    <div className=" items-center   px-11 py-14 text-2xl same-bg mt-16 justify-center">
      <DocsSingleBox  Documents={Documents}/>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import SecondPage from "../pages/SecondPage";
 import { useEffect } from "react";
 import "lenis/dist/lenis.css";
 import Lenis from "lenis";
+import Docs from "../app/docs/page";
 export default function Home() {
   useEffect(() => {
     const lenis = new Lenis();
@@ -24,6 +25,10 @@ export default function Home() {
     <div className=" bg-[#080D27]   py-16">
       <LandingPage />
       <SecondPage />
+      <div className="ml-36">
+        <div className="text-[#C2C9F3] text-[4rem] text-center -mb-10 mr-60">Docs</div>
+        <Docs />
+      </div>
     </div>
   );
 }
