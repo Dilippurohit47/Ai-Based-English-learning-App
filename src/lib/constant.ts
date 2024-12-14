5da9a7ce9be02ffb5a70d059eb24ccdafbe0a640
Dilippurohit47 <dilippurohit204@gmail.com>
2024-12-14 22:20:50 +0530

    docs changed

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,27 +1,27 @@
 type docType = {
-    title: string;
-    desc: string;
-  };
-  
-  export const Documents: docType[] = [
-    {
-      title: "Donate to me so I can afford to ship this product.",
-      desc: "I currently don't have the funds to host this project, which is why    it's not yet live. If I receive enough support through donations, I'll  be able to host the project and make it available for everyone,   Additionally, with your help, I can integrate a more advanced AI API,  small contributions will go a long way in helping me bring this  project to life!  ",
-    },
-    {
-      title: "How to Get Started?",
-      desc: "I currently don't have the funds to host this project, which is why    it's not yet live. If I receive enough support through donations, I'll  be able to host the project and make it available for everyone,   Additionally, with your help, I can integrate a more advanced AI API,  small contributions will go a long way in helping me bring this  project to life!  ",
-    },
-    {
-      title: "What are the Features?",
-      desc: "I currently don't have the funds to host this project, which is why    it's not yet live. If I receive enough support through donations, I'll  be able to host the project and make it available for everyone,   Additionally, with your help, I can integrate a more advanced AI API,  small contributions will go a long way in helping me bring this  project to life!  ",
-    },
-    {
-      title: "Can it speak and listen our voice also?",
-      desc: "I currently don't have the funds to host this project, which is why    it's not yet live. If I receive enough support through donations, I'll  be able to host the project and make it available for everyone,   Additionally, with your help, I can integrate a more advanced AI API,  small contributions will go a long way in helping me bring this  project to life!  ",
-    },
-    {
-      title: "Which Ai Api is used in backend to fetch conversations?.",
-      desc: "I currently don't have the funds to host this project, which is why    it's not yet live. If I receive enough support through donations, I'll  be able to host the project and make it available for everyone,   Additionally, with your help, I can integrate a more advanced AI API,  small contributions will go a long way in helping me bring this  project to life!  ",
-    },
-  ];
+  title: string;
+  desc: string;
+};
+
+export const Documents: docType[] = [
+  {
+    title: "Donate to me so I can afford to ship this product.",
+    desc: "I currently don't have the funds to host this project, which is why    it's not yet live. If I receive enough support through donations, I'll  be able to host the project and make it available for everyone,   Additionally, with your help, I can integrate a more advanced AI API,  small contributions will go a long way in helping me bring this  project to life!  ",
+  },
+  {
+    title: "How to Get Started?",
+    desc: "First, log in and click on Demo for Free. You will be directed to a screen where you can type and converse with AI to enhance your English skills through guided interaction and feedback.",
+  },
+  {
+    title: "What are the Features?",
+    desc: "Features include speech-to-text and text-to-speech. Users can talk with AI, and the AI will respond, providing a comprehensive way to practice and improve English.",
+  },
+  {
+    title: "Can it speak and listen our voice also?",
+    desc: "yes it can speak and listen to our voice. ",
+  },
+  {
+    title: "Which Ai Api is used in backend to fetch conversations?.",
+    desc: "GeminiAi api which is free so not to good but it works sometimes . Dont judge my project on ai responses ",
+  },
+];
